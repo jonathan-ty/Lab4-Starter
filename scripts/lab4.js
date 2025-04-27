9b9e2d981c1f69ad7ca0d65d0bd06795bcb7c380
Jonathan Ty <jonathanty42@gmail.com>
2025-04-27 11:25:11 -0700

    sumValues else block will return false.

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -1,43 +1,42 @@
 /**
- * 
- * @param {*} num1, first number to add. 
- * @param {*} num2, second number to add. 
- * @param {*} add, boolean value to tell the function what to do. 
+ *
+ * @param {*} num1, first number to add.
+ * @param {*} num2, second number to add.
+ * @param {*} add, boolean value to tell the function what to do.
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
-    if (add & (typeof num1 === "number") && (typeof num2 === "number")) {
-        let result = 0;
+  if (add & (typeof num1 === "number") && typeof num2 === "number") {
+    let result = 0;
 
-        result = num1 + num2;
+    result = num1 + num2;
 
-        return result;
-    }
-    else {
-        return add;
-    }
+    return result;
+  } else {
+    return false;
+  }
 }
 
 /**
- * 
+ *
  * @param {*} prices, an array of the original price.
- * @param {*} discount, a number between 0-1 to represent the discount. 
+ * @param {*} discount, a number between 0-1 to represent the discount.
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
-    const discounted = []
-    const length = prices.length;
-    let discountedPrice = 0;
-    if ((typeof prices !== "object") | (length === 0)) {
-      return false;
-    }
-    for(let i = 0; i < length; i++) {
-        discountedPrice = prices[i] * (1 - discount);
-        discounted.push(discountedPrice);
-    }
+  const discounted = [];
+  const length = prices.length;
+  let discountedPrice = 0;
+  if ((typeof prices !== "object") | (length === 0)) {
+    return false;
+  }
+  for (let i = 0; i < length; i++) {
+    discountedPrice = prices[i] * (1 - discount);
+    discounted.push(discountedPrice);
+  }
 
-    return discounted;
+  return discounted;
 }
 
 console.log(discountPrices([10, 20, 30], 0.1)); // [80, 160, 240]
-module.exports = {sumValues, discountPrices};
+module.exports = { sumValues, discountPrices };
